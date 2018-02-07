bece84766cb19274310a8cfea6671970fa5e8b34
Alex <alexavetisov@outlook.com>
2018-02-07 17:23:04 -0600

    Added in user option for type of characters in password

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-#!/bin/python3
 import random
-
-chars = 'ABCDEFGHIJKLMNOPQRSTUVabcdefghijklmnopqrstuvwxyz1234567890!#$%&()*+,-./<=>?@[]\^_`{|}~'
 
 length = input('How many characters would you like in your password?: ')
 length = int(length)
 
+choice = input('Enter \'1\' for just letters and numbers, or type \'2\' to also include special characters. ')
+choice = int(choice)
+
+if choice == 1:
+  chars = 'ABCDEFGHIJKLMNOPQRSTUVabcdefghijklmnopqrstuvwxyz1234567890'
+
+elif choice == 2:
+  chars = 'ABCDEFGHIJKLMNOPQRSTUVabcdefghijklmnopqrstuvwxyz1234567890!#$%&()*+,-./<=>?@[]\^_`{|}~'
+
+else:
+  print("Invalid Choice");
+  
 password = ''
 
 for c in range(length):
